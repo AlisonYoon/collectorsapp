f20c0afb802f7702d47190417d42158134003ec6
AlisonYoon <politescarlett1@gmail.com>
2019-08-05 15:16:57 +0100

    price and remaining data set to unsigned

--- collectorsapp_2019-08-05.sql
+++ collectorsapp_2019-08-05.sql
@@ -7,7 +7,7 @@
 #
 # Host: 192.168.20.20 (MySQL 5.6.44)
 # Database: collectorsapp
-# Generation Time: 2019-08-05 14:10:06 +0000
+# Generation Time: 2019-08-05 14:15:21 +0000
 # ************************************************************
 
 
@@ -27,10 +27,10 @@ DROP TABLE IF EXISTS `grocery_item`;
 
 CREATE TABLE `grocery_item` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `item` varchar(255) DEFAULT NULL,
+  `item` varchar(255) NOT NULL DEFAULT '',
   `category` enum('PANTRY','FRIDGE','FREEZER','PRODUCE','OTHER') NOT NULL,
   `price` int(11) unsigned NOT NULL,
-  `remaining` int(11) DEFAULT NULL,
+  `remaining` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -43,9 +43,9 @@ DROP TABLE IF EXISTS `grocery_to_do`;
 
 CREATE TABLE `grocery_to_do` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `item` varchar(255) DEFAULT NULL,
-  `category` enum('PANTRY','FRIDGE','FREEZER','PRODUCE','OTHER') DEFAULT NULL,
-  `price` int(11) DEFAULT NULL,
+  `item` varchar(255) NOT NULL DEFAULT '',
+  `category` enum('PANTRY','FRIDGE','FREEZER','PRODUCE','OTHER') NOT NULL,
+  `price` int(11) unsigned DEFAULT NULL,
   `from` enum('CO-OP','FRUIT STAND','SCOOPAWAY','KOREAN MARKET','FISH MARKET','AMAZON') DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
