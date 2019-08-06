59509ab78d6c550acb32bee07c42501faa95042a
AlisonYoon <politescarlett1@gmail.com>
2019-08-06 14:48:04 +0100

    getData, processData functions in separate file

---
+++ function.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'db.php';
+
+function getData($db) {
+    $db->setAttribute(
+        PDO::ATTR_DEFAULT_FETCH_MODE,
+        PDO::FETCH_ASSOC);
+
+    $sql = $db->prepare('SELECT `item`, `category`, `price`, `remaining` FROM `grocery_item`');
+    $sql->execute();
+    $groceryItems = $sql->fetchAll();
+
+    return $groceryItems;
+}
+
+function processData($groceryItems) {
+    $itemRow= '';
+    foreach($groceryItems as $item) {
+        $itemRow .= '<div><ul><li>' . $item["item"] . '</li><li>' . $item['category'] . '</li><li>' . $item['price'] . '</li><li>' . $item['remaining'] . '</li></ul></div>';
+    }
+    return $itemRow;
+}
+
+
+//function call
+
+$db = connectDB();
+$groceryItems = getData($db);
+$groceryItemsArray = processData($groceryItems);
+
+
+?>
