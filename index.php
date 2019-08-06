86e93bf831cf4f00de2486b6637175237c0f91d3
AlisonYoon <politescarlett1@gmail.com>
2019-08-06 19:09:44 +0100

    processData returns HTML unit, added input form and some styling added for header

--- index.php
+++ index.php
@@ -18,8 +18,40 @@ $groceryItemsArray = processData($groceryItems);
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-<h1>Grocery Items</h1>
+<header>
+    <ul>
+        <li><a href="">All</a></li>
+        <li><a href="">Pantry</a></li>
+        <li><a href="">Fridge</a></li>
+        <li><a href="">Freezer</a></li>
+        <li><a href="">Produce</a></li>
+        <li><a href="">Other</a></li>
+    </ul>
+</header>
+<section>
+    <form action="index.php" method="get">
+        <input type="text" name="item" placeholder="item">
+        <select name="category">
+            <option value="pantry">Pantry</option>
+            <option value="fridge">Fridge</option>
+            <option value="freezer">Freezer</option>
+            <option value="produce">Produce</option>
+            <option value="other">Other</option>
+        </select>
+        <input type="text" name="price" placeholder="price">
+        <input type="text" name="remaining" placeholder="remaining">
+        <input type="submit">
+    </form>
+</section>
 
+<div class="table-heading">
+    <ul>
+        <li>Item</li>
+        <li>Category</li>
+        <li>Price(pence)</li>
+        <li>Remaining(%)</li>
+    </ul>
+</div>
 
 <?php
  echo $groceryItemsArray;
