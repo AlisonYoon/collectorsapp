99c0e3f7088c8fc020c536c8da4c8032b586acab
AlisonYoon <politescarlett1@gmail.com>
2019-08-06 14:49:09 +0100

    require function file and print it as html

--- index.php
+++ index.php
@@ -1,23 +1,6 @@
 <?php
 
-$db = new PDO('mysql:host=192.168.20.20; dbname=collectorsapp', 'root', '');
-$db->setAttribute(
-    PDO::ATTR_DEFAULT_FETCH_MODE,
-    PDO::FETCH_ASSOC);
-
-$sql = 'SELECT * FROM `grocery_item`';
-$sql = $db->prepare('SELECT * FROM `grocery_item`;');
-$sql->execute();
-$groceryItem = $sql->fetchAll();
-
-//var_dump($groceryItem);
-
-foreach($groceryItem as $item) {
-    echo $item["item"] . ' | ' . $item['category'] . ' | ' . $item['price'] . ' | ' . $item['remaining'];
-    echo '<br>';
-}
-
-
+require_once 'function.php';
 
 ?>
 <!DOCTYPE html>
@@ -27,8 +10,11 @@ foreach($groceryItem as $item) {
 
 </head>
 <body>
-    <form action="index.php" method="get">
+<p style="color: red"> lalalalal</p>
 
-    </form>
+
+<?php
+ echo $groceryItemsArray;
+?>
 </body>
 </html>
