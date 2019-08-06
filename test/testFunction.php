426e2b8193817c0bac8cc205c10762eff727f6cb
AlisonYoon <politescarlett1@gmail.com>
2019-08-06 20:42:32 +0100

    generateRequest with Param test passed

--- test/testFunction.php
+++ test/testFunction.php
@@ -11,6 +11,16 @@ class FunctionTest extends TestCase
         $result = generateRequest();
         $expectedResult = 'SELECT `item`, `category`, `price`, `remaining` FROM `grocery_item`';
 
+        //assertion
+        $this->assertEquals($result, $expectedResult);
+    }
+
+    public function testGenerateRequestShouldReturnWhereCategoryWithParam()
+    {
+        //setup
+        $filter = 'pantry';
+        $result = generateRequest($filter);
+        $expectedResult = 'SELECT `item`, `category`, `price`, `remaining` FROM `grocery_item` WHERE `category` = "' . $filter.  '";';
 
         //assertion
         $this->assertEquals($result, $expectedResult);
