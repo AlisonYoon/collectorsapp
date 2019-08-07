af9cc79a52d4a54f9aad1604e729bf2b1bc03dd5
AlisonYoon <politescarlett1@gmail.com>
2019-08-07 09:11:15 +0100

    category column font style changed, form style changed

--- function.php
+++ function.php
@@ -41,7 +41,7 @@ function generateRequest(string $filter = 'all'):string {
 function processData(array $groceryItems):string {
     $itemRow= '';
     foreach($groceryItems as $item) {
-        $itemRow .= '<div class="item-row"><ul><li>' . $item["item"] . '</li><li>' . $item['category'] . '</li><li>' . $item['price'] . '</li><li>' . $item['remaining'] . '</li></ul></div>';
+        $itemRow .= '<div class="item-row"><ul><li>' . $item["item"] . '</li><li class="' .$item["category"] . '-row">' . $item['category'] . '</li><li>' . $item['price'] . '</li><li>' . $item['remaining'] . '</li></ul></div>';
     }
     return $itemRow;
 }
