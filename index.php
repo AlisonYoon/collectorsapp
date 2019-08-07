a23a9e3a1e6eb12ac435df57203bac9793b8b5d5
AlisonYoon <politescarlett1@gmail.com>
2019-08-07 14:10:34 +0100

    media query for Add new item form works

--- index.php
+++ index.php
@@ -24,9 +24,34 @@ $groceryItemsArray = processData($groceryItems);
         <?php echo currentFilter($category) ?>
     </ul>
 </header>
-<section>
 
-    <form action="index.php" method="get">
+<section>
+    <!--  Form for media query smaller screen  -->
+    <div class="tabs">
+        <div class="tab">
+            <input type="checkbox" id="tab">
+            <label class="tab-label" for="tab">Add a new item</label>
+            <div class="tab-content">
+                <form action="index.php" method="get">
+                    <input type="text" name="item" placeholder="item" required>
+                    <select name="category" required>
+                        <option value="" disabled selected hidden>Category</option>
+                        <option value="pantry">Pantry</option>
+                        <option value="fridge">Fridge</option>
+                        <option value="freezer">Freezer</option>
+                        <option value="produce">Produce</option>
+                        <option value="other">Other</option>
+                    </select>
+                    <input type="text" name="price" placeholder="price (pence)" required>
+                    <input type="text" name="remaining" placeholder="remaining (%)" required>
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+        </div>
+    </div>
+    <!--  Media query form for smaller screen end   -->
+
+    <form action="index.php" method="get" class="big-form">
         <p>Add a New Item</p>
         <input type="text" name="item" placeholder="item" required>
         <select name="category" required>
